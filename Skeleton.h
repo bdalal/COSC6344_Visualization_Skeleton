d272ab553a30c9648a229e2d0589513f9d1c04ae
binoy.dalal <binoy.dalal@lucidworks.com>
2018-11-07 20:08:44 -0600

    fixed formatting and poly properties

--- Skeleton.h
+++ Skeleton.h
@@ -23,65 +23,65 @@ Modified by Guoning Chen 2012
 //using namespace std;
 
 const double EPS = 1.0e-6;
-const double PI=3.1415926535898;
+const double PI = 3.1415926535898;
 
 /* forward declarations */
 class Triangle;
 
 class Vertex {
 public:
-  double x,y,z;
-  float s;
-  float vx, vy, vz;
-  int index;
+	double x, y, z;
+	int angle_deficit;
+	float nx, ny, nz;
+	int index;
 
-  int ntris;
-  Triangle **tris;
-  int max_tris;
+	int ntris;
+	Triangle **tris;
+	int max_tris;
 
 	icVector3 normal;
-  void *other_props;
+	void *other_props;
 
 public:
-  Vertex(double xx, double yy, double zz) { x = xx; y = yy; z = zz; }
+	Vertex(double xx, double yy, double zz) { x = xx; y = yy; z = zz; }
 };
 
 class Edge {
 public:
-  int index;
-  Vertex *verts[2];
-  int ntris;
-  Triangle **tris;
+	int index;
+	Vertex *verts[2];
+	int ntris;
+	Triangle **tris;
 	double length;
 };
 
 class Triangle {
 public:
-  int index;
-  int nverts;
-  Vertex *verts[3];
-  Edge *edges[3];
+	int index;
+	int nverts;
+	Vertex *verts[3];
+	Edge *edges[3];
 
 	double angle[3];
 	float area;
 
 	icVector3 normal;
-  void *other_props;
+	void *other_props;
 };
 
 class VertexList {
 
 public:
 
-  int num_verts;
-  int max_verts;
-  Vertex **verts;
+	int num_verts;
+	int max_verts;
+	Vertex **verts;
 
-  VertexList (int max) {
-    max_verts = max;
-    verts = new Vertex *[max_verts];
-    num_verts = 0;
-  }
+	VertexList(int max) {
+		max_verts = max;
+		verts = new Vertex *[max_verts];
+		num_verts = 0;
+	}
 	void finalize() {
 		if (this != NULL) {
 			free(verts);
@@ -114,15 +114,15 @@ public:
 class TriangleList {
 public:
 
-  int num_tris;
-  int max_tris;
-  Triangle **tris;
+	int num_tris;
+	int max_tris;
+	Triangle **tris;
 
-  TriangleList (int max) {
-    max_tris = max;
-    tris = new Triangle *[max_tris];
-    num_tris = 0;
-  }
+	TriangleList(int max) {
+		max_tris = max;
+		tris = new Triangle *[max_tris];
+		num_tris = 0;
+	}
 	void append(Triangle *t)
 	{
 		int i;
@@ -149,15 +149,15 @@ class EdgeList {
 
 public:
 
-  int num_edges;
-  int max_edges;
-  Edge **edges;
+	int num_edges;
+	int max_edges;
+	Edge **edges;
 
-  EdgeList (int max) {
-    max_edges = max;
-    edges = new Edge *[max_edges];
-    num_edges = 0;
-  }
+	EdgeList(int max) {
+		max_edges = max;
+		edges = new Edge *[max_edges];
+		num_edges = 0;
+	}
 	void append(Edge *e)
 	{
 		int i;
@@ -185,17 +185,17 @@ public:
 
 	int index;
 
-  Triangle **tlist;  /* list of triangles */
-  int ntris;
-  int max_tris;
+	Triangle **tlist;  /* list of triangles */
+	int ntris;
+	int max_tris;
 
-  Vertex **vlist;    /* list of vertices */
-  int nverts;
-  int max_verts;
+	Vertex **vlist;    /* list of vertices */
+	int nverts;
+	int max_verts;
 
-  Edge **elist;      /* list of edges */
-  int nedges;
-  int max_edges;
+	Edge **elist;      /* list of edges */
+	int nedges;
+	int max_edges;
 
 	icVector3 center;
 	double radius;
@@ -203,27 +203,27 @@ public:
 
 	int seed;
 
-  //PlyOtherProp *vert_other,*face_other;
-	
+	//PlyOtherProp *vert_other,*face_other;
+
 	Polyhedron();
-    Polyhedron(FILE *);
+	Polyhedron(FILE *);
 
 	void average_normals();
 
-  void create_edge(Vertex *, Vertex *);
-  void create_edges();
-  int face_to_vertex_ref(Triangle *, Vertex *);
-  void order_vertex_to_tri_ptrs(Vertex *);
-  void vertex_to_tri_ptrs();
-  Triangle *find_common_edge(Triangle *, Vertex *, Vertex *);
-  Triangle *other_triangle(Edge *, Triangle *);
+	void create_edge(Vertex *, Vertex *);
+	void create_edges();
+	int face_to_vertex_ref(Triangle *, Vertex *);
+	void order_vertex_to_tri_ptrs(Vertex *);
+	void vertex_to_tri_ptrs();
+	Triangle *find_common_edge(Triangle *, Vertex *, Vertex *);
+	Triangle *other_triangle(Edge *, Triangle *);
 	void calc_bounding_sphere();
 	void calc_face_normals_and_area();
 	void calc_edge_length();
 
-  void write_file(FILE *);
+	void write_file(FILE *);
 
-  void create_pointers();
+	void create_pointers();
 
 	// initialization and finalization
 	void initialize();
